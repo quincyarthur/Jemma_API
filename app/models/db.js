6184a05aa9168d5d6b3ccb19e2e82e4e2651cf8e
Quincy Arthur <quincya@nib-bahamas.com>
2017-10-07 12:33:08 -0400

    deploy

--- app/models/db.js
+++ app/models/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Sequelize = require('sequelize'); 
 
 if (process.env.NODE_ENV === 'production'){
-    var sequelize = new Sequelize(process.env.DATABASE_URL,{dialect:'postgres'});
+    var sequelize = new Sequelize(process.env.DATABASE_URL,{dialect:'postgres',define:{underscored:true}});
 }
 else{
     var sequelize = 
